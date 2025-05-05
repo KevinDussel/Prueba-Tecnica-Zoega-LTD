2f0b23772e488ede568db916137981fdab406cd0
KevinDussel <151534929+KevinDussel@users.noreply.github.com>
2025-05-05 11:38:14 +0200

    react from react

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext.tsx'
 import './index.css'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
